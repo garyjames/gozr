8a608b7a8df9d380760068fc409fe929d521511f
Gary Galvez <ggalvez@wolve.com>
2015-02-11 17:17:10 -0600

    Added execution and cleaned up parseline

---
+++ execution.py
@@ -0,0 +1,12 @@
+# execution.py
+
+def execution(fixmsg, tags={'ClOrdID': '11', 'LastShares': '32', 'Symbol': '55'}):
+    # identify an execution from the fixmsg tuple
+    tag_values = {}
+    for tag in tags:
+        for i in fixmsg:
+            if i.startswith(tags[tag]+'='):
+                tag_values[tag] = i.split('=')[1]
+    if len(tag_values) == len(tags):
+        return tag_values
+
--- parseline.py
+++ parseline.py
@@ -1,3 +1,4 @@
 def parseline(line, idx_l='8=FIX', idx_r='\x01', SOH='\x01'):
-    rec = line[line.find(idx_l) : line.rfind(idx_r)].split(SOH)
-    return tuple(rec)
+    rec = line[line.find(idx_l) : line.rfind(idx_r)]
+    if rec:
+        return tuple(rec.split(SOH))
