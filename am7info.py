8df4c7f983d78d46ab69809884cea00e943ba3ac
Gary Galvez <ggalvez@wolve.com>
2015-03-23 17:18:38 -0500

    tweaked am7info

--- am7info.py
+++ am7info.py
@@ -9,9 +9,7 @@ def myfunc():
             with open(filename,'r') as fh:
                 soup = bs(fh)
             print '{}'.format(fh.name)
-            print soup.prettify()
-            print
-            ret = soup.findAll(lambda x: x.has_attr('OUTPUTFILE'))
+            ret = soup.findAll(lambda x: x.has_attr('file') and 'ython' in x['file'])
             for i in ret:
                 print ret
             print
