ad569ec604136129ca40fbaf2f1cbc8db86349e2
Gary James <garygalvez@gmail.com>
2015-03-23 21:29:02 -0500

    tweaked am7info

--- am7info.py
+++ am7info.py
@@ -3,16 +3,25 @@ import sys
 from bs4 import BeautifulSoup as bs
 
 def myfunc():
-    _d = {}
+
     for filename in os.listdir(os.curdir):
-        if '.aml' in filename:
-            with open(filename,'r') as fh:
+        if filename.endswith('.aml'):
+            modified = False
+            with open(filename) as fh:
                 soup = bs(fh)
-            print '{}'.format(fh.name)
-            ret = soup.findAll(lambda x: x.has_attr('file') and 'ython' in x['file'])
+            print '{}'.format(filename)
+            ret = soup.findAll(
+                  lambda x: x.has_attr('file') and 'python' in x['file']
+            )
+
             for i in ret:
-                print ret
-            print
+                if not i.has_attr('foo'):
+                    i['FOO'] = 'bar'
+                    modified = True
+
+            if modified:
+                with open(filename + '.new', 'w') as fh:
+                    fh.write(soup.prettify())
 
 if __name__ == "__main__":
     myfunc()
