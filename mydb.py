aea35dcdb1e46ac79553a308bd5297e22f37f53f
Gary Galvez <ggalvez@wolve.com>
2015-04-10 13:51:02 -0500

    some changes to class mydb
    Trying to keep making this as generic as possible, but we still have a
    long way to go.

--- mydb.py
+++ mydb.py
@@ -2,29 +2,61 @@
 """db server connection"""
 
 import pyodbc
-from exceptions import StandardError
 from collections import namedtuple
-import mydsn as dsn
+import utilities as utils
 
-class MyDB(object):
+pyodbc.paramstyle = 'pyformat'
+
+class mydb(object):
     """more info at https://www.python.org/dev/peps/pep-0249"""
 
-    apilevel = '2.0'
-    threadsafety = 1
-    paramstyle = 'pyformat'
-
-    _tmps = 'Driver={{{driver}}};Server={server};Database={database};Uid={uid};Pwd={pwd}'
-
     def __init__(self, db_alias):
-        self._dsn = _tmps.format(**dsn.dsn(db_alias))
+        tmp = 'Driver={{{driver}}};Database={database};Server={server};Uid={uid};Pwd={pwd}'
+        ret = utils.dsn(db_alias)
+        self._dsn = tmp.format(**ret)
         self._cnxn = pyodbc.connect(self._dsn)
         self._cursor = self._cnxn.cursor()
+        self._db = self._cnxn.getinfo(pyodbc.SQL_DATABASE_NAME)
+        self._server = self._cnxn.getinfo(pyodbc.SQL_SERVER_NAME)
+        self._username = self._cnxn.getinfo(pyodbc.SQL_USER_NAME)
 
     def __del__(self):
         self._cnxn.close()
 
-    def query(self, query, params):
-        return self._cursor.execute(query, params)
+    def query(self, query):
+        return self._cursor.execute(query)
+
+    @property
+    def cursor(self):
+        return self._cursor
+
+    @property
+    def username(self):
+        self._username = self._cnxn.getinfo(pyodbc.SQL_USER_NAME)
+        return self._username
+
+    @property
+    def server(self):
+        self._server = self._cnxn.getinfo(pyodbc.SQL_SERVER_NAME)
+        return self._server
+
+    @property
+    def db(self):
+        self._db = self._cnxn.getinfo(pyodbc.SQL_DATABASE_NAME)
+        return self._db
+
+    @db.setter
+    def db(self, d):
+        self._cursor.execute("use {}".format(d))
+        return self._cnxn.getinfo(pyodbc.SQL_DATABASE_NAME)
+
+    @property
+    def rowcount(self):
+        return self._cursor.rowcount
+
+    @property
+    def description(self):
+        return self._cursor.description
 
     @property
     def databases(self):
@@ -32,10 +64,9 @@ class MyDB(object):
 
     @property
     def tables(self):
-        if self._db:
-            return self.query(
-                "select table_name from %(db)s.information_schema.tables " +
-                "where table_type = 'BASE TABLE'", db=self._db)
+        q = ("select table_name from {}.information_schema.tables "
+             "where table_type = 'BASE TABLE'")
+        return self.query(q.format(self._db))
 
 #    def query(self, query=None, ret_as_tuple=True):
 #        """return either a cursor or a generator which yields named tuples"""
@@ -46,3 +77,7 @@ class MyDB(object):
 #            return ( named_row._make(i) for i in records )
 #        elif query:
 #            return records
+
+def firms(cnxn):
+    q = r"select * from firms"
+    return cnxn.query(q)
