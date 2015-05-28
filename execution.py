01995f77555737f1f9132bae4e1fafd060f4f2aa
Gary James <garygalvez@gmail.com>
2015-05-27 23:14:53 -0500

    Update execution.py
    that line will never be False, because the key will be overwritten if a duplicate is present

--- execution.py
+++ execution.py
@@ -7,6 +7,6 @@ def execution(fixmsg, tags={'ClOrdID': '11', 'LastShares': '32', 'Symbol': '55'}
         for i in fixmsg:
             if i.startswith(tags[tag]+'='):
                 tag_values[tag] = i.split('=')[1]
-    if len(tag_values) == len(tags):
-        return tag_values
+    #if len(tag_values) == len(tags):
+    return tag_values
 
