599e678d69a940c8e8051495f0f3fb620caf0eb6
Gary James <garygalvez@gmail.com>
2015-06-01 22:28:01 -0500

    tweaked base_encode

--- base_encode.py
+++ base_encode.py
@@ -1,9 +1,9 @@
-# base 32 encode
+# base 32 codex
 
 map32 = ( 
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L',
-    'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'W', 'X', 'Y', 'Z'
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
+    'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'W', 'X',
+    'Y', 'Z'
 )
 
 map32 = dict(enumerate(map32))
@@ -14,13 +14,17 @@ def encode32(n, b32=str(), exp=0):
     else:
         return encode32(n/32, b32=map32[n%32]+b32, exp=exp+1)
 
-def decode32(s):
-    result = 0
-    exp = len(s)
-    
-    pass
+def decode32(s, ):
+    exp = len(s) - 1
+    if exp == 0:
+        return result + s
+    else:
+        return
 
 if __name__ == '__main__':
     from sys import argv
-    n = int(argv[1])
-    print encode32(n)
+    n = argv[1]
+    if isinstance(n, int):
+        print encode32(n)
+    else:
+        decode32(n)
