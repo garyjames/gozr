30b4987eadb731e629930086409b8082878b1595
garyjames <garygalvez@gmail.com>
2017-02-18 19:54:19 -0600

    some fixin

--- fieldparse.py
+++ fieldparse.py
@@ -5,10 +5,18 @@ def parse(lines, types, names=None, sep=None):
     Parse a line of column oriented data into a list of dictionaries
     or tuples with type conversion.
     '''
-    records = []
+
+    from collections import namedtuple
+
+    if names:
+        records = namedtuple('records', names)
+    else:
+        records = []
+
     for line in lines:
         line = line.strip()
-        if not line: continue
+        if not line:
+            continue
         fields = line.split(sep)
         
         # Get rid of double quotes
@@ -19,7 +27,7 @@ def parse(lines, types, names=None, sep=None):
 
         # Optionally turn into a dictionary of named fields
         if names is not None:
-            record = dict(zip(names,cfields))
+            records(zip(names,cfields))
         else:
             record = tuple(cfields)
 
