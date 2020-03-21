d075cd21afb2ba2801679cc22e2f520b72747375
Gary James Galvez <garyjames>
2020-03-20 21:00:15 -0500

    updated test_bin_builder

--- casino/tests/test_bin_builder.py
+++ casino/tests/test_bin_builder.py
@@ -1,7 +1,5 @@
 import unittest
-from roulette.game import Odds
-from roulette.outcome import Outcome
-from roulette.binbuilder import BinBuilder
+from src.roulette import Odds, Outcome, BinBuilder
 
 
 class defaultSetup(unittest.TestCase):
@@ -13,18 +11,18 @@ class defaultSetup(unittest.TestCase):
         self.col3_nums = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
 
         self.bins = [set() for _ in range(38)]
-        for n, oc in enumerate(self.builder.split_outcomes()):
-            self.bins[n] |= oc
-        for n, oc in enumerate(self.builder.straight_outcomes()):
-            self.bins[n] |= oc
-        for n, oc in enumerate(self.builder.street_outcomes()):
-            self.bins[n] |= oc
-        for n, oc in enumerate(self.builder.corner_outcomes()):
-            self.bins[n] |= oc
-        for n, oc in enumerate(self.builder.line_outcomes()):
-            self.bins[n] |= oc
-        for n, oc in enumerate(self.builder.dozen_outcomes()):
-            self.bins[n] |= oc
+        for n, oc in self.builder.split():
+            self.bins[n] |= {oc}
+        for n, oc in self.builder.straight():
+            self.bins[n] |= {oc}
+        for n, oc in self.builder.street():
+            self.bins[n] |= {oc}
+        for n, oc in self.builder.corner():
+            self.bins[n] |= {oc}
+        for n, oc in self.builder.line():
+            self.bins[n] |= {oc}
+        for n, oc in self.builder.dozen():
+            self.bins[n] |= {oc}
 
 
 class TestColumnGenerators(defaultSetup):
@@ -32,16 +30,6 @@ class TestColumnGenerators(defaultSetup):
         self.assertTrue(self.col1_nums, range(1, 35, 3))
         self.assertTrue(self.col2_nums, range(2, 36, 3))
         self.assertTrue(self.col3_nums, range(3, 37, 3))
-
-    def test_generators(self):
-        self.assertEqual([i for i in self.builder.col_1s], self.col1_nums)
-        self.assertEqual([i for i in self.builder.col_2s], self.col2_nums)
-        self.assertEqual([i for i in self.builder.col_3s], self.col3_nums)
-
-
-class ConfirmTmpBinsLength(defaultSetup):
-    def test_length(self):
-        self.assertTrue(len(self.builder.tmp_bins()) == 38)
 
 
 class TestStraightOdds(defaultSetup):
@@ -96,29 +84,11 @@ class TestDozen(defaultSetup):
         self.assertTrue(Outcome("Dozen 3", self.odds.dozen) in self.bins[36])
 
 
-class AreColumnsReallyColumns(defaultSetup):
-    def setUp(self) -> None:
-        self.builder = BinBuilder()
-        self.columns = self.builder.column()
-        self.odds = Odds()
-
-    def test_0(self):
-        for n in range(1, 35, 3):
-            self.assertEqual(Outcome("Column {}".format(1), self.odds.column).win_amount(10), 20)
-
-    def test_1(self):
-        for n in self.builder.col_1s:
-            self.assertIn(Outcome("Column {}".format(1), self.odds.column), self.columns[n])
-
-    def test_2(self):
-        for n in self.builder.col_2s:
-            self.assertIn(Outcome("Column {}".format(2), self.odds.column), self.columns[n])
-
 
 class TestAllOutcomes(defaultSetup):
     def test_street(self):
         oc_name = "Street"
-        for row in self.builder.col_1s:
+        for row in self.col1_nums:
             for n in [row, row + 1, row + 2]:
                 self.assertIn(Outcome("{} {}-{}-{}".format(oc_name, row, row + 1, row + 2),
                                       self.odds.street), self.bins[n])
@@ -129,7 +99,8 @@ class TestAllOutcomes(defaultSetup):
             for i in [0, 1, 3, 4]:
                 self.assertIn(
                     Outcome("{} {}-{}-{}-{}".format(oc_name, row, row + 1, row + 3, row + 4), self.odds.corner),
-                    self.bins[row + i])
+                    self.bins[row + i]
+                   )
         for row in range(2, 33, 3):
             for i in [0, 1, 3, 4]:
                 self.assertIn(
@@ -148,10 +119,10 @@ class TestAllOutcomes(defaultSetup):
         self.assertIn(Outcome("Straight {}".format('00'), self.odds.straight), self.bins[37])
 
     def test_splits(self):
-        for i in self.builder.col_1s:
+        for i in self.col1_nums:
             self.assertIn(Outcome("Split {}-{}".format(i, i + 1), self.odds.split), self.bins[i])
             self.assertIn(Outcome("Split {}-{}".format(i, i + 1), self.odds.split), self.bins[i + 1])
-        for i in self.builder.col_2s:
+        for i in self.col2_nums:
             self.assertIn(Outcome("Split {}-{}".format(i, i + 1), self.odds.split), self.bins[i])
             self.assertIn(Outcome("Split {}-{}".format(i, i + 1), self.odds.split), self.bins[i + 1])
 
